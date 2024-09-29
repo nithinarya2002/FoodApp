569c11566ce5aa80bb43e50683b03f2d7477c6ea
nithinarya2002 <nithinarya2002@gmail.com>
2024-09-29 13:48:41 +0530

    onClick event listener

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,11 +4,56 @@ import resList from "./utils/mockData";
 
 
 const Body = () => {
+
+    let listOfRestaurants = [
+    {
+        info: {
+        id: "150598",
+        name: "Begam Bazar Dosa",
+        cloudinaryImageId: "fm3rs3g6z7ibfhesmxnu",
+        costForTwo: "₹200 for two",
+        cuisines: ["Bakery", "Ice Cream", "Snacks", "Beverages"],
+        avgRating: 3.5,
+        sla: {
+          deliveryTime: 73,
+        }
+      }
+    },
+    {
+        info: {
+        id: "150597",
+        name: "Scoops Fast Food And Ice Cream",
+        cloudinaryImageId: "fm3rs3g6z7ibfhesmxnu",
+        costForTwo: "₹200 for two",
+        cuisines: ["Bakery", "Ice Cream", "Snacks", "Beverages"],
+        avgRating: 4,
+        sla: {
+          deliveryTime: 73,
+        }
+      }
+    },
+    {
+        info: {
+        id: "150599",
+        name: "kadak Chai",
+        cloudinaryImageId: "fm3rs3g6z7ibfhesmxnu",
+        costForTwo: "₹200 for two",
+        cuisines: ["Bakery", "Ice Cream", "Snacks", "Beverages"],
+        avgRating: 4.5,
+        sla: {
+          deliveryTime: 73,
+        }
+      }
+    }]
+
     return (
       <div className="body">
-        <div className="search-bar">Search bar</div>
+        <button className="filter-btn" onClick={()=>{
+            listOfRestaurants = listOfRestaurants.filter((resinfo)=>resinfo.info.avgRating<4);
+            console.log(listOfRestaurants);
+        }}>Top rated Restaurants</button>
         <div className="res-container">
-          {resList.map((restinfo) => (
+          {listOfRestaurants.map((restinfo) => (
             <ResCard key={restinfo.info.id} resData={restinfo} />
           ))}
         </div>
