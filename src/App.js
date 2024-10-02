d3859ac5718e9e6c58328f8ef8b7a8bd35d5f464
nithinarya2002 <nithinarya2002@gmail.com>
2024-10-02 23:11:27 +0530

    Used lazy component to optimize the code and create different bundles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Error from "./components/Error";
 import ResMenuCard from "./components/ResMenuCard";
+// import Grocery from "./components/Grocery";
+import { lazy } from "react";
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const Contact = lazy(()=>import("./components/Contact"));
 
 const AppLayout = () => {
   return (
@@ -32,13 +37,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: <Suspense fallback={<h1>Loading.....</h1>}>
+        <Contact />
+      </Suspense>,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading.....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       //Colon indicates dynamic path
       {
-        path:"/restaurant/:resId",
-        element:<ResMenuCard />
-      }
+        path: "/restaurant/:resId",
+        element: <ResMenuCard />,
+      },
     ],
     errorElement: <Error />,
   },
