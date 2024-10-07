7beacd0e337d2cec38400601f8f718c5e13162e7
nithinarya2002 <nithinarya2002@gmail.com>
2024-10-07 23:30:25 +0530

    Good UI

--- src/components/ShimmerMenuContainer.js
+++ src/components/ShimmerMenuContainer.js
@@ -1,17 +1,21 @@
 import ShimmerMenu from "./ShimmerMenu";
-const ShimmerMenuContainer = ()=>{
-    return (
-      <div className = "mt-12">
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-        <ShimmerMenu />
-      </div>
-    );
+const ShimmerMenuContainer = () => {
+  return (
+    <div className="mt-12">
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+      <ShimmerMenu />
+    </div>
+  );
 };
 
 export default ShimmerMenuContainer;
